f10433105b695ba14f5e4888bb1a1397696bf846
alleycaaat <achulslander@gmail.com>
2024-04-25 12:33:19 -0500

    adding files

---
+++ src/components/Body.jsx
@@ -0,0 +1,110 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect, useRef } from 'react';
+
+import { HiChevronDoubleUp } from 'react-icons/hi';
+
+import Skills from './Skills';
+import Education from './Education';
+//import Articles from './Articles';
+import Portfolio from './Portfolio';
+import FullPortfolio from './FullPortfolio';
+import Credit from './Credit';
+import { AboutInfo } from './UI/AboutInfo';
+
+const MainBody = ({ setIsModalOpen, section, showCredit, setShowCredit }) => {
+    const ref = useRef();
+    const [onscreen, setonscreen] = useState(false);
+    const [fullPortfolioHidden, setFullPortfolioHidden] = useState(true);
+
+    useEffect(() => {
+        if (section !== undefined) {
+            if (section === 'resume' || section === 'home') {
+                return;
+            }
+            document.getElementById(`${ section }`).scrollIntoView({ behavior: 'smooth' });
+        }
+        else return;
+    }, [section]);
+
+    /* ! need to alter scrollIntoView for users preferring reduced motion */
+    const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (mediaQuery && !mediaQuery.matches) {
+                handleScroll();
+            }
+        });
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    });
+
+    const handleScroll = () => {
+        if (ref.current.getBoundingClientRect().top <= window.innerHeight) {
+            setonscreen(true);
+        }
+    };
+
+    const iconStyle = {
+        color: '#000',
+        height: '25px',
+        width: '25px',
+        backgroundColor: '',
+        ariaHidden: 'true',
+        border: '2px solid black',
+        borderRadius: '50%',
+        marginTop: '15px',
+    };
+
+    const ReturnToTop = () => {
+        return (
+            <button className='returnBtn'>
+                <span className='sr-only'>Return to about section</span>
+                <HiChevronDoubleUp style={iconStyle} onClick={() => ref.current.scrollIntoView({ behavior: 'smooth' })} />
+            </button>
+        );
+    };
+    return (
+        <div className='column' role='main' ref={ref}>
+            {showCredit ? <Credit setShowCredit={setShowCredit} />
+                :
+                <>
+                    <section className='about' id='about' aria-label='about AC'>
+                        <div className='desc'>
+                            <div className='sec'>
+                                <figure>
+                                    <img
+                                        src='https://ach-photos.netlify.app/ACHulslanderProfile.jpg'
+                                        loading='lazy'
+                                        alt='AC on a sunny day standing next to her horse, Aspen, giving her a kiss on the side of her muzzle'
+                                        className={onscreen ? 'aspen scroll scrolled' : 'aspen scroll'}
+                                    />
+                                    <figcaption>
+                                        <strong>Wait, who?</strong>
+                                    </figcaption>
+                                </figure>
+                                <p>
+                                    I'm AC, that's Aspen in the photo with me. I've been making websites since the days of over-animated personal Geocities pages.  Nowadays, the animations I incoporate in my designs are fun accents rather than an overwhelming eyesore.  My passion and skills for web development and design have grown in leaps and bounds since my first Geocities (RIP) account was created.
+                                </p>
+                            </div>
+                            <AboutInfo />
+                        </div>
+                    </section>
+                    <Skills id='skills' />
+                    <ReturnToTop />
+
+                    <Education id='education' />
+                    <ReturnToTop />
+
+                    {fullPortfolioHidden && (<Portfolio id='portfolio' setIsModalOpen={setIsModalOpen} setFullPortfolioHidden={setFullPortfolioHidden} />)}
+                    {!fullPortfolioHidden && <FullPortfolio setFullPortfolioHidden={setFullPortfolioHidden} />}
+
+                    <ReturnToTop />
+                </>
+            }
+        </div>
+    );
+};
+
+export default MainBody;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,160 @@
+import { useState, useEffect, useRef } from 'react';
+
+const Footer = ({ section, setShowCredit }) => {
+    const ref = useRef();
+    const [onscreen, setonscreen] = useState(false);
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const handleScroll = () => {
+        if (ref.current.getBoundingClientRect().top <= window.innerHeight) {
+            setonscreen(true);
+        }
+    };
+
+    useEffect(() => {
+        if (section === 'contact') {
+            ref.current.scrollIntoView({ behavior: 'smooth' });
+        }
+        else return;
+    }, [section]);
+
+    const icons = [
+        {
+            name: 'codepen',
+            url: 'https://codepen.io/alleycaaat',
+            img: 'codepen_icon',
+            sr: 'External link to AC`s codepen',
+        },
+        {
+            name: 'twitter',
+            url: 'https://twitter.com/alleycaaat',
+            img: 'twitter_icon',
+            sr: 'External link to AC`s twitter',
+        },
+        {
+            name: 'hashnode',
+            url: 'https://blog.achulslander.com/',
+            img: 'hashnode',
+            sr: 'External link to AC`s blog',
+        },
+        {
+            name: 'GitHub',
+            url: 'https://github.com/alleycaaat',
+            img: 'github_icon',
+            sr: 'External link to AC`s GitHub',
+        },
+    ];
+    return (
+        <footer>
+            <section ref={ref} className='contact' id='contact'>
+                <h3 aria-label='contact'>Let's talk!</h3>
+                <div className='socialIconsContact'>
+                    <ul>
+                        {icons.map((icon, i) => (
+                            <li className='tooltip--triangle footer' data-tooltip={`${ icon.name }`} key={i}>
+                                <span className='hex-shape'>
+                                    <a href={icon.url}
+                                        rel='noopener noreferrer'
+                                        target='_blank'>
+                                        <img
+                                            aria-hidden='true'
+                                            loading='lazy'
+                                            className='icon'
+                                            src={`https://ach-photos.netlify.app/${ icon.img }.svg`}
+                                            alt={`${ icon.name } logo`}
+                                        />
+                                        <span className='visually-hidden'>
+                                            {icon.sr}
+                                        </span>
+                                        <span className='tooltiptext'>{icon.name}</span>
+                                    </a>
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <form
+                    className='contact-form'
+                    method='POST'
+                    id='contactForm'
+                    name='contactMe'
+                    aria-label='Send AC a message'
+                >
+                    <input type='hidden' name='form-name' value='contactMe' />
+                    <div className='col'>
+                        <label hidden htmlFor='inputName'>Name</label>
+                        <input
+                            type='text'
+                            className={onscreen ? 'form-element text scroll scrolled' : 'form-element text scroll'}
+                            style={{ animationDelay: '.3s' }}
+                            id='inputName'
+                            name='inputName'
+                            placeholder='Name *'
+                            required
+                        />
+                        <label hidden htmlFor='inputEmail'>Email address</label>
+                        <input
+                            type='email'
+                            placeholder='Email Address *'
+                            className={onscreen ? 'form-element text scroll scrolled' : 'form-element text scroll'}
+                            style={{ animationDelay: '.6s' }}
+                            id='inputEmail'
+                            name='inputEmail'
+                            required
+                        />
+                        <label hidden htmlFor='inputMessage'>Type your message here</label>
+                        <textarea
+                            className={onscreen ? 'form-element textarea scroll scrolled' : 'form-element textarea scroll'}
+                            style={{ animationDelay: '.9s' }}
+                            placeholder='Type your message here *'
+                            id='inputMessage'
+                            name='inputMessage'
+                            rows='5'
+                            required
+                        ></textarea>
+                        <input
+                            type='submit'
+                            className={onscreen ? 'btn sendBtn scroll scrolled' : 'btn sendBtn scroll'}
+                            style={{ animationDelay: '1.2s' }}
+                            id='screen'
+                            value='Send'
+                        />
+                        <div id='html_element'></div>
+                        <input
+                            type='submit'
+                            className={onscreen ? 'btn sendBtn scroll scrolled' : 'btn sendBtn scroll'}
+                            style={{ animationDelay: '1.5s' }}
+                            id='mobile'
+                            value='Send'
+                        />
+                    </div>
+                </form>
+                <p>Content &#169; 2021 - 2024 AC Hulslander</p>
+                <p>
+                    <a
+                        className='link'
+                        href='https://www.freeprivacypolicy.com/live/9f85fba1-b648-47e8-9675-18cda2045b75'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >Privacy Policy</a>{' '}|{' '}
+                    <a
+                        className='link'
+                        href='https://www.freeprivacypolicy.com/live/cb7a0887-a6fd-43d9-83f3-1dd79dc2f54e'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        Terms & Conditions</a>{' '}|{' '}
+                    <button className='link' onClick={() => setShowCredit(true)}>Icon Sources</button>
+                </p>
+            </section>
+        </footer>
+    );
+};
+
+export default Footer;
